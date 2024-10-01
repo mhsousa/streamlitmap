d561b7aa7af56e3436a3ad0b0d08fb3cc39ed616
Mario Henrique <102007914+mhsousa@users.noreply.github.com>
2024-09-30 23:46:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,5 +11,27 @@ df=pd.DataFrame({
     'cor':np.random.rand(4, 4).tolist()
 })
 
+df2=pd.DataFrame({
+    'cidade': ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Salvador'],
+    'lat': [-23.5505, -22.9083, -19.9208, -12.9704],
+    'lon': [-46.6333, -43.1728, -43.9389, -38.5019],
+    'populacao': [412912.3, 6226444.7, 33112.5, 547722.9],
+    'cor':np.random.rand(4, 4).tolist()
+})
+
+df3=pd.DataFrame({
+    'cidade': ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Salvador'],
+    'lat': [-23.5505, -22.9083, -19.9208, -12.9704],
+    'lon': [-46.6333, -43.1728, -43.9389, -38.5019],
+    'populacao': [1912.3, 3226444.7, 3533112.5, 37722.9],
+    'cor':np.random.rand(4, 4).tolist()
+})
+
 st.title("Teste mapa scatter")
-st.map(df, size="populacao", color="cor", zoom=3)
+c1, c2=st.columns(2)
+with c1:
+    st.map(df, size="populacao", color="cor", zoom=3)
+with c2:
+    st.map(df2, size="populacao", color="cor", zoom=3)
+    
+    st.map(df3, size="populacao", color="cor", zoom=3)
